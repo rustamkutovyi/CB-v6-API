d71aebf1332153720235be6ea8a46f81131cec71
Rustam Kutovyi <137978032+rustamkutovyi@users.noreply.github.com>
2024-05-23 20:50:44 -0700

    Auth (#4)
    * add auth postivie test
    
    * added env
    
    * second commit for env

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -1,13 +1,14 @@
 import request from 'supertest';
 import { expect } from 'chai';
+import 'dotenv/config'
 
 describe('AUTHORIZATION AND AUTHENTICATION', () => {
   it('login with valid credentials', async () => {
-    const res = await request('https://clientbase-server.herokuapp.com/v6')
+    const res = await request(process.env.BASE_URL)
       .post('/user/login')
       .send({
-        email: 'jeff1@martin.com',
-        password: 'Password1',
+        email: process.env.EMAIL,
+        password: process.env.PASSWORD,
       });
       expect(res.statusCode).to.eq(200)
       expect(res.body.message).to.eq('Auth success')
