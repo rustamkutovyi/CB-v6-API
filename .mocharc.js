4bb37bb3d7033fc4cdc2837b23d8ebbcf48c0a6d
rustamkutovyi <rustamkutovoy996@gmail.com>
2024-06-19 13:57:58 -0700

    add reporter

--- .mocharc.js
+++ .mocharc.js
@@ -2,5 +2,6 @@ module.exports = {
     require: ['@babel/register'],
     timeout: 5000,
     exclude:['test/example.spec.js'],
-    file: 'config/project-setup.js'
+    file: 'config/project-setup.js',
+    reporter: 'mochawesome'
 }
